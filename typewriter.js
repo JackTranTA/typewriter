6c2b721fd772404f81730cbeac2aa583594a0a53
Jack Tran <bluestar113311@gmail.com>
2023-08-03 07:44:32 -0700

    Added and finished typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+
+for (const char of sentence) {
+  delay += 50;
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay) // <= 1s delay to make it noticeable. Can dial it down later.
+}
+
+console.log();
